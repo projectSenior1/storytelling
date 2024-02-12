f9e6995e27c2ca0ccd2feff901b76feb98607a4f
Khalil420777 <bouarroujkhalil@gmail.com>
2024-02-12 13:12:58 +0100

    final12

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,7 +15,8 @@ import Cookies from "js-cookie"
 const Home = ({id}) => {
   const [data, setData] = useState([]);
   const[category,setCategory]=useState([])
-
+  const[user,setUser]=useState("")
+  const[userstory,setUserstory]=useState([])
   const [search, setSearch] = useState(""); 
   const [filter,setFilter]=useState([])
   const[select,setSelect]=useState([])
@@ -54,6 +55,19 @@ const Home = ({id}) => {
 
   
   }
+// const getoneuser=()=>{
+//   axios.get(`http://localhost:5000/users/getoneuser/${id}`).then((res)=>{
+//     setUser(res.users)
+//     axios.get(`http://localhost:5000/story/getuserstory/${id}`).then((result)=>{
+//       setUserstory(result.userstory)
+//       navigate("/UserStories")
+//     })
+//     .catch((err)=>{
+//       console.log(err,"erroror");
+//     })
+
+//   })
+// }
 
 
   const handleSearch = (e) => {
@@ -89,10 +103,16 @@ setSelect(filtered)
           <li className='active'> 
             <Link to="/home" className="home">Home</Link>
           </li>
-          <li className="search-bar">
-            <input type="text" placeholder="Search..." onChange={handleSearch} value={search} />
-            <button className='sbutton'>Search</button>
-          </li>
+          <div className="group">
+  <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+    <g>
+      <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+    </g>
+  </svg>
+  <input className="input" type="search" placeholder="Search" onChange={handleSearch} value={search} />
+  {/* <div onClick={()=>{getoneuser()}}>< input type="search" /></div> */}
+</div>
+
           <li className="category-bar">
         
             <select  onChange={(e)=>{
